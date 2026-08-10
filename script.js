ea90b05ef328c4deb0839be08ec9d79f06db4816
xyyzl2026 <18229815802@163.com>
2026-08-10 23:49:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const message = document.querySelector('#message');
 const greetings = [
   '很高兴见到你，愿你今天灵感满满！',
   '谢谢来访，希望这个小页面能带给你一点好心情。',
-  '保持好奇，继续创造属于你的精彩。',
+  '保持热爱，继续创造属于你的精彩。',
 ];
 
 let greetingIndex = 0;
